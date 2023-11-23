42fd6a338ddcbd146fdd4f4905ffef592bbdc4c6
Alejandro Pedraza <luisalejandropedrazacubillos@student.mitt.ca>
2023-11-23 11:42:25 -0600

    Create the basic structure

--- assets/script/classes.js
+++ assets/script/classes.js
@@ -1,2 +1,20 @@
 'use strict';
 
+class Shape {
+    constructor(name, color) {
+        this._name = name;
+        this._color = color;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    getInfo() {
+        return `Shape: ${this._name}, Color: ${this._color}`;
+    }
+}
